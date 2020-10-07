c261b240dac9ad990a6defe356d05d7d0ad16973
HSgod <hsgodkontakt@gmail.com>
2020-10-07 18:45:36 +0000

    sqlite: Add missing initializers after 3.33.0 update
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- dist/shell.c
+++ dist/shell.c
@@ -5714,6 +5714,10 @@ static sqlite3_module seriesModule = {
   0,                         /* xRollback */
   0,                         /* xFindMethod */
   0,                         /* xRename */
+  0,                         /* xSavepoint */
+  0,                         /* xRelease */
+  0,                         /* xRollbackTo */
+  0,                         /* xShadowName */
 };
 
 #endif /* SQLITE_OMIT_VIRTUALTABLE */
